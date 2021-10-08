2c514311390f0035b4f4dcd53c7fcc0ebebf7d9f
user <user@LAPTOP-OITE8GBV>
2021-10-08 16:43:19 +0800

    test

---
+++ testJedis/src/test/java/testJedis/testJedis/TestJedis.java
@@ -0,0 +1,10 @@
+package testJedis.testJedis;
+import redis.clients.jedis.Jedis;
+public class TestJedis {
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("localhost");
+		jedis.set("foo", "chw");
+		String value = jedis.get("foo");
+		System.out.print(value);
+	}
+} 
